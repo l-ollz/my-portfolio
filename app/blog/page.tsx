cd0c4649b4e7340672feb0ecde3d4a45a169104b
l-ollz <hasmoto.0112@gmail.com>
2024-08-14 16:27:54 +0900

    microCMS blog取得

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,53 +1,33 @@
 import Link from 'next/link';
-import { Suspense } from 'react';
-import ViewCounter from './view-counter';
-import { getViewsCount } from 'app/db/queries';
-import { getBlogPosts } from 'app/db/blog';
-
-export const metadata = {
-  title: 'Blog',
-  description: 'Read my thoughts on software development, design, and more.',
-};
-
-export default function BlogPage() {
-  let allBlogs = getBlogPosts();
+import { getList } from 'libs/microcms';
+export default async function BlogPage() {
+  const { contents } = await getList();
 
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
         read my blog
       </h1>
-      {allBlogs
+      {contents
         .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
+          if (new Date(a.createdAt) > new Date(b.createdAt)) {
             return -1;
           }
           return 1;
         })
         .map((post) => (
           <Link
-            key={post.slug}
+            key={post.id}
             className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
+            href={`/blog/${post.id}`}
           >
             <div className="w-full flex flex-col">
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
+                {post.title}
               </p>
-              <Suspense fallback={<p className="h-6" />}>
-                <Views slug={post.slug} />
-              </Suspense>
             </div>
           </Link>
         ))}
     </section>
   );
-}
-
-async function Views({ slug }: { slug: string }) {
-  let views = await getViewsCount();
-
-  return <ViewCounter allViews={views} slug={slug} />;
 }
--- app/blog/view-counter.tsx
+++ app/blog/view-counter.tsx
@@ -1,16 +1,16 @@
 export default function ViewCounter({
-  slug,
+  id,
   allViews,
 }: {
-  slug: string;
+  id: string;
   allViews: {
-    slug: string;
+    id: string;
     count: number;
   }[];
   trackView?: boolean;
 }) {
-  const viewsForSlug = allViews && allViews.find((view) => view.slug === slug);
-  const number = new Number(viewsForSlug?.count || 0);
+  const viewsForId = allViews && allViews.find((view) => view.id === id);
+  const number = new Number(viewsForId?.count || 0);
 
   return (
     <p className="text-neutral-600 dark:text-neutral-400">
